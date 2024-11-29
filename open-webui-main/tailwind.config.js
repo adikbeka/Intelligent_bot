672e83aa210030f56b011c0dbfadcb3b1ddf61d3
Adlet Bekbergenov <adletbek9@gmail.com>
2024-11-29 09:37:36 +0800

    a

--- open-webui-main/tailwind.config.js
+++ open-webui-main/tailwind.config.js
@@ -12,11 +12,9 @@ export default {
 					300: '#cdcdcd',
 					400: '#b4b4b4',
 					500: '#9b9b9b',
-					600: '#676767',
 					700: '#4e4e4e',
 					800: 'var(--color-gray-800, #333)',
 					850: 'var(--color-gray-850, #262626)',
-					900: 'var(--color-gray-900, #171717)',
 					950: 'var(--color-gray-950, #0d0d0d)'
 				}
 			},
