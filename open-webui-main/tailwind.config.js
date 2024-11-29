fc60e03071bbab03c5a5e986542f67db79d86abe
Adlet Bekbergenov <adletbek9@gmail.com>
2024-11-29 09:41:22 +0800

    Configured tailwind config, added theme change for 900, changed background size, padding and repeat

--- open-webui-main/tailwind.config.js
+++ open-webui-main/tailwind.config.js
@@ -11,10 +11,12 @@ export default {
 					200: '#e3e3e3',
 					300: '#cdcdcd',
 					400: '#b4b4b4',
+					600: '#676767',
 					500: '#9b9b9b',
 					700: '#4e4e4e',
 					800: 'var(--color-gray-800, #333)',
 					850: 'var(--color-gray-850, #262626)',
+					900: 'var(--color-gray-900, #171717)',
 					950: 'var(--color-gray-950, #0d0d0d)'
 				}
 			},
